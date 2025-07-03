a36d76d80b1b400af8c652fd7af7f8c27be5a446
Karolis Ramanauskas <karolisr@gmail.com>
2025-07-02 19:44:12 -0500

    template_widget cleanup.

--- riced/src/template_widget.rs
+++ riced/src/template_widget.rs
@@ -1,10 +1,17 @@
 use iced::{
     Element, Length, Rectangle, Size, Vector,
     advanced::{
-        self, Clipboard, Shell, layout, mouse, overlay, renderer,
-        widget::{self, tree},
+        Clipboard, Renderer as RendererTrait, Shell,
+        layout::{Layout, Limits, Node},
+        mouse::{Cursor, Interaction as MouseInteraction},
+        overlay,
+        renderer::Style,
+        widget::{
+            Operation, Widget,
+            tree::{State, Tag, Tree},
+        },
     },
-    event,
+    event::Event,
 };
 
 pub struct TheWidget {
@@ -12,65 +19,62 @@ pub struct TheWidget {
     height: Length,
 }
 
-impl<Message, Theme, Renderer> widget::Widget<Message, Theme, Renderer> for TheWidget
+impl<Message, Theme, Renderer> Widget<Message, Theme, Renderer> for TheWidget
 where
-    Renderer: advanced::Renderer,
+    Renderer: RendererTrait,
 {
-    fn size(&self) -> Size<Length> { Size { width: self.width, height: self.height } }
-
-    fn layout(
-        &self, _tree: &mut tree::Tree, _renderer: &Renderer, limits: &layout::Limits,
-    ) -> layout::Node {
-        layout::Node::new(limits.min())
-    }
-
-    fn draw(
-        &self, _tree: &tree::Tree, _renderer: &mut Renderer, _theme: &Theme,
-        _style: &renderer::Style, _layout: layout::Layout<'_>, _cursor: mouse::Cursor,
+    fn update(
+        &mut self, _tree: &mut Tree, _event: &Event, _layout: Layout<'_>, _cursor: Cursor,
+        _renderer: &Renderer, _clipboard: &mut dyn Clipboard, _shell: &mut Shell<'_, Message>,
         _viewport: &Rectangle,
     ) {
     }
 
-    fn tag(&self) -> tree::Tag { tree::Tag::stateless() }
-
-    fn state(&self) -> tree::State { tree::State::None }
-
-    fn children(&self) -> Vec<tree::Tree> { Vec::new() }
-
-    fn diff(&self, tree: &mut tree::Tree) { tree.children.clear(); }
-
-    fn operate(
-        &self, _state: &mut tree::Tree, _layout: layout::Layout<'_>, _renderer: &Renderer,
-        _operation: &mut dyn widget::Operation,
-    ) {
+    fn layout(&self, _tree: &mut Tree, _renderer: &Renderer, limits: &Limits) -> Node {
+        Node::new(limits.min())
     }
 
-    fn update(
-        &mut self, _state: &mut tree::Tree, _event: &event::Event, _layout: layout::Layout<'_>,
-        _cursor: mouse::Cursor, _renderer: &Renderer, _clipboard: &mut dyn Clipboard,
-        _shell: &mut Shell<'_, Message>, _viewport: &Rectangle,
+    fn draw(
+        &self, _tree: &Tree, _renderer: &mut Renderer, _theme: &Theme, _style: &Style,
+        _layout: Layout<'_>, _cursor: Cursor, _viewport: &Rectangle,
     ) {
-    }
-
-    fn mouse_interaction(
-        &self, _state: &tree::Tree, _layout: layout::Layout<'_>, _cursor: mouse::Cursor,
-        _viewport: &Rectangle, _renderer: &Renderer,
-    ) -> mouse::Interaction {
-        mouse::Interaction::None
     }
 
     fn overlay<'a>(
-        &'a mut self, _state: &'a mut tree::Tree, _layout: layout::Layout<'a>,
-        _renderer: &Renderer, _viewport: &Rectangle, _translation: Vector,
+        &'a mut self, _tree: &'a mut Tree, _layout: Layout<'a>, _renderer: &Renderer,
+        _viewport: &Rectangle, _translation: Vector,
     ) -> Option<overlay::Element<'a, Message, Theme, Renderer>> {
         None
     }
+
+    fn mouse_interaction(
+        &self, _tree: &Tree, _layout: Layout<'_>, _cursor: Cursor, _viewport: &Rectangle,
+        _renderer: &Renderer,
+    ) -> MouseInteraction {
+        MouseInteraction::None
+    }
+
+    fn operate(
+        &self, _tree: &mut Tree, _layout: Layout<'_>, _renderer: &Renderer,
+        _operation: &mut dyn Operation,
+    ) {
+    }
+
+    fn size(&self) -> Size<Length> { Size { width: self.width, height: self.height } }
+
+    fn tag(&self) -> Tag { Tag::stateless() }
+
+    fn state(&self) -> State { State::None }
+
+    fn children(&self) -> Vec<Tree> { Vec::new() }
+
+    fn diff(&self, tree: &mut Tree) { tree.children.clear(); }
 }
 
 impl<'a, Message, Theme, Renderer> From<TheWidget> for Element<'a, Message, Theme, Renderer>
 where
     Message: 'a,
-    Renderer: advanced::Renderer + 'a,
+    Renderer: RendererTrait + 'a,
 {
     fn from(w: TheWidget) -> Self { Element::new(w) }
 }
