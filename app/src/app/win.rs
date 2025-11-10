9e3628282380605d59a557284a61143f10cdd022
Karolis Ramanauskas <karolisr@gmail.com>
2025-11-10 07:35:30 -0600

    Updates the default window settings.

--- app/src/app/win.rs
+++ app/src/app/win.rs
@@ -8,30 +8,38 @@ use super::consts::APP_ID;
 
 pub fn window_settings() -> WindowSettings {
     WindowSettings {
+        maximized: true,
+        fullscreen: false,
+        resizable: true,
+        position: WindowPosition::Centered,
         size: Size { width: 900.0 * SF, height: 600.0 * SF },
         min_size: Some(Size { width: 900.0, height: 600.0 }),
-        position: WindowPosition::Centered,
-        resizable: true,
+        max_size: None,
+        icon: None,
+        decorations: true,
+        exit_on_close_request: false,
         level: WindowLevel::Normal,
+        transparent: false,
+        visible: true,
+
         #[cfg(target_os = "macos")]
         platform_specific: PlatformSpecificWindowSettings {
             title_hidden: false,
             titlebar_transparent: false,
             fullsize_content_view: false,
         },
+
         #[cfg(target_os = "windows")]
         platform_specific: PlatformSpecificWindowSettings {
             drag_and_drop: true,
             skip_taskbar: false,
             undecorated_shadow: true,
         },
+
         #[cfg(target_os = "linux")]
         platform_specific: PlatformSpecificWindowSettings {
             application_id: String::from(APP_ID),
             override_redirect: false,
         },
-        exit_on_close_request: false,
-        decorations: true,
-        ..Default::default()
     }
 }
